b03a694e08a67d1240d10406c225748935cb5b2b
sal1800 <sal1800@gmail.com>
2025-03-29 11:21:19 -0700

    Added APU Node, added clear nodes function, added import nodes function, tank node can now select curves from a list

--- src/components/nodes/tankNode.vue
+++ src/components/nodes/tankNode.vue
@@ -19,7 +19,20 @@
                 <el-input v-model="unusuableCapacity" df-unusablecapacity size="small"></el-input>
             </el-form-item>
             <el-form-item label="Curve" label-position="left">
-                <el-input v-model="curve" df-curve size="small"></el-input>
+                <el-select
+                  v-model="curve"
+                  placeholder="Select"
+                  style="width: 100%"
+                  clearable
+                  @change="setOption"
+                >
+                  <el-option
+                    v-for="item in curveList"
+                    :key="item"
+                    :label="item.name"
+                    :value="item.index"
+                  />
+                </el-select>
             </el-form-item>
             <el-form-item label="Position" label-position="left">
                 <el-input v-model="position" df-position size="small"></el-input>
@@ -54,9 +67,26 @@ export default defineComponent({
         const capacity = ref(0);
         const unusuableCapacity = ref(0);
         const curve = ref('');
+        const curveList = ref([]);
         const droptimer = ref('');
         const position = ref('0,0,0');
         const priority = ref('1');
+
+        const setOption = () => {
+            nextTick( () => {
+                const data = { curve: curve.value, ...dataNode.value.data };
+                df.updateNodeDataFromId(nodeId.value, data);
+            });
+        }
+
+        const getCurves = () => {
+            const exportdata = df.export();
+            const curves = Object.entries(exportdata.drawflow.Home.data).filter(([key,node]) => node.class === 'Curve');
+            if (curves) {
+                curveList.value = curves.map(([k,c]) => ({name:c.data.name, index: c.data.index}));
+            }
+        }
+
 
         df = getCurrentInstance().appContext.config.globalProperties.$df.value;
     
@@ -64,6 +94,9 @@ export default defineComponent({
             await nextTick()
             nodeId.value = el.value.parentElement.parentElement.id.slice(5)
             dataNode.value = df.getNodeFromId(nodeId.value)
+
+            df.on('nodeCreated', getCurves);
+            df.on('nodeRemoved', getCurves);         
             
             itemIndex.value = dataNode.value.data.index;
             itemName.value = dataNode.value.data.itemname;
@@ -73,10 +106,12 @@ export default defineComponent({
             curve.value = dataNode.value.data.curve;
             position.value = dataNode.value.data.position;
             priority.value = dataNode.value.data.priority;
+
+            getCurves();
         });
         
         return {
-            el, itemIndex, itemName, itemTitle, capacity, unusuableCapacity, curve, position, priority, droptimer
+            el, itemIndex, itemName, itemTitle, capacity, unusuableCapacity, curve, position, priority, droptimer, curveList
         }
 
     }    
